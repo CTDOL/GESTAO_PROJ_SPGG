2a9be504d8c27caffcddd9da9501dd39666e0e13
Jonas Henrique Spindler <198004216+jonasCTDOL@users.noreply.github.com>
2026-08-18 12:33:32 -0300

    fix: CORS headers for X-Last-Modified

--- public/api.php
+++ public/api.php
@@ -5,7 +5,8 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *'); // Permite conexões locais para dev
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Content-Type, X-Last-Modified');
+header('Access-Control-Expose-Headers: X-Last-Modified');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
